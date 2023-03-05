468d7b21f231be5085bc598dfd634a2d302cba5f
mariakingcilin <mariakingcilin@gmail.com>
2023-03-05 12:03:12 +0530

    add

---
+++ main.js
@@ -0,0 +1,66 @@
+//(------for-array-sumof------)
+// var array=[1,2,3,4,5];
+// var total=0;
+// for(i=0;i<array.length;i++){
+	// total+=array[i];
+	// }
+	// document.write(total);
+	// document.write("<br>");
+//(------while-sumof------)
+// var i=1;
+// var n=6
+// var total=0;
+// while(i<=n){
+	// total+=i;
+	// i++;
+	// }
+	// document.write(total);
+	// document.write("<br>");
+//(------while-reverse------)
+// var i=6;
+// var n=0;
+// while(i>n){
+	// document.write(i);
+	// document.write("<br>");
+	// i--;
+	// }
+//(------for-reverse------)
+// var n=0;
+// for(i=6;i>n;i--){
+	// document.write(i);
+	// document.write("<br>");
+	// }
+//(------for-reverse------)
+var n=6;
+for(i=1;i<=n;i++){
+	if (i<=5){
+		document.write(i);
+	}
+	// document.write(i);
+	// document.write("<br>");
+	}
+	document.write("<br>");
+for(i=1;i<=n;i++){
+	if (i<=4){
+		document.write(i);
+	}
+	}
+	document.write("<br>");
+for(i=1;i<=n;i++){
+	if (i<=3){
+		document.write(i);
+	}
+	}
+	document.write("<br>");
+for(i=1;i<=n;i++){
+	if (i<=2){
+		document.write(i);
+	}
+	}
+	document.write("<br>");
+for(i=1;i<=n;i++){
+	if (i<=1){
+		document.write(i);
+	}
+	}
+	
